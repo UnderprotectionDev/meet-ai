ff11327079ae36f77e24498001dd4e7a4a70a0a7
Underprotection <mertcandemirkaya@outlook.com>
2025-09-02 15:36:14 +0300

    20: Meeting Variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -13,6 +13,10 @@ import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { useRouter } from "next/navigation";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface MeetingIdViewProps {
   meetingId: string;
@@ -49,6 +53,12 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
     await removeMeeting.mutate({ id: meetingId });
   };
 
+  const isActive = data.status === "active";
+  const isUpcoming = data.status === "upcoming";
+  const isCancelled = data.status === "cancelled";
+  const isCompleted = data.status === "completed";
+  const isProcessing = data.status === "processing";
+
   return (
     <>
       <RemoveConfirmation />
@@ -64,7 +74,11 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
           onEdit={() => setUpdateMeetingDialogOpen(true)}
           onRemove={handleRemoveMeeting}
         />
-        {JSON.stringify(data, null, 2)}
+        {isCancelled && <CancelledState />}
+        {isProcessing && <ProcessingState />}
+        {isCompleted && <p>Completed</p>}
+        {isActive && <ActiveState meetingId={meetingId} />}
+        {isUpcoming && <UpcomingState meetingId={meetingId} />}
       </div>
     </>
   );
