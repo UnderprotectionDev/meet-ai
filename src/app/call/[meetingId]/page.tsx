0a13a1deb6562f0d3abce44b7c8a568f02628cf7
Underprotection <mertcandemirkaya@outlook.com>
2025-09-02 22:02:41 +0300

    21: Video Call

---
+++ src/app/call/[meetingId]/page.tsx
@@ -0,0 +1,39 @@
+import { auth } from "@/lib/auth";
+import { CallView } from "@/modules/call/ui/views/call-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface CallPageProps {
+  params: Promise<{ meetingId: string }>;
+}
+
+export default async function CallPage({ params }: CallPageProps) {
+  const { meetingId } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error</p>}>
+          <CallView meetingId={meetingId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+}
